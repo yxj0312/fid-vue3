c5bd01b50b18f331c679b7cb636c032d1908a87e
yxj0312 <yanxiajun@hotmail.com>
2020-10-23 22:53:25 +0200

    Add product component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,13 +38,28 @@
       <h3 class="text-gray-600 text-2xl font-medium">
         Fashions
       </h3>
+      <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Product from '../components/Product.vue';
+
 export default {
   name: 'Home',
+  components: {
+    Product,
+  },
 
 };
 </script>
