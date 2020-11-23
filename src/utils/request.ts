c8c5535210a36b7ac99c7fcbcd4102d4bf7b8ed2
yxj0312 <yanxiajun@hotmail.com>
2020-11-23 23:10:26 +0100

    Add a constructor to request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,2 +1,21 @@
 import merge from 'deepmerge'
 
+export interface FetchRequestOptions {
+  prefix: string
+  headers: Record<string, string>
+  params: Record<string, string | number | boolean>
+}
+export default class FetchRequest{
+    private readonly defaultOptions: FetchRequestOptions = {
+        prefix: '',
+        headers: {},
+        params: {},
+    }
+
+    private readonly options: FetchRequestOptions
+
+    constructor (options: Partial<FetchRequestOptions> = {}) {
+        this.options = merge(this.defaultOptions, options)
+    }
+}
+
