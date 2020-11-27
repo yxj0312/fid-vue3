1807cd205ee88ff87dd3d252d756d296369bfc53
yxj0312 <yanxiajun@hotmail.com>
2020-11-27 20:53:13 +0100

    Update getProducts

--- src/services/product/getProducts.ts
+++ src/services/product/getProducts.ts
@@ -1,7 +1,7 @@
 import { request } from '../index'
 
 export function getArticles (page = 1): Promise<ProductResponse> {
-//   const params = { limit, offset: (page - 1) * limit }
-//   const params = null
+  const params = { limit, offset: (page - 1) * limit }
+  //   const params = null
   return request.get<ProductResponse>('/fid-task-4-ffront-products.json')
 }
