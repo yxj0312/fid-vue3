5e3871b404aa3e064019ed4b65c6ba62b0966703
yxj0312 <yanxiajun@hotmail.com>
2020-11-30 23:18:46 +0100

    Add async process

--- src/utils/create-async-process.ts
+++ src/utils/create-async-process.ts
@@ -5,3 +5,16 @@ interface CreateAsyncProcessReturn<T extends (...args: any[]) => any> {
   active: Ref<boolean>
   run: (...args: Parameters<T>) => Promise<ReturnType<T>>
 }
+
+export default function CreateAsyncProcess<T extends (...args: any[]) => any> (fn: T): CreateAsyncProcessReturn<T> {
+  const active: CreateAsyncProcessReturn<T>['active'] = ref(false)
+
+  const run: CreateAsyncProcessReturn<T>['run'] = async (...args) => {
+    active.value = true
+    const result = await fn(...args)
+    active.value = false
+    return result
+  }
+
+  return { active, run }
+}
