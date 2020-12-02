efa83f8cafd3667397d21aa9354b1afd85fabf36
yxj0312 <yanxiajun@hotmail.com>
2020-12-02 23:04:39 +0100

    fetchprodudct

--- src/composable/useProducts.ts
+++ src/composable/useProducts.ts
@@ -2,3 +2,19 @@ import { computed, ComputedRef, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 import createAsyncProcess from '../utils/create-async-process'
+
+import { getProducts } from '../services/product/getProducts'
+
+export function useProducts () {
+  const products = ref<Product[]>([])
+
+  async function fetchProducts (): Promise<void> {
+    products.value = []
+    let responsePromise: null | Promise<ProductsResponse> = null
+
+    responsePromise = getProducts()
+    if (responsePromise !== null) {
+      const response = await responsePromise
+    }
+  }
+}
