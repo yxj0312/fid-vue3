661e74be623eeb00eaffa87621ef2ba8ff747ca2
yxj0312 <yanxiajun@hotmail.com>
2020-12-05 17:35:37 +0100

    Try to remove page parm in getProducts

--- src/services/product/getProducts.ts
+++ src/services/product/getProducts.ts
@@ -1,6 +1,6 @@
 import { request } from '../index'
 
-export function getProducts (page = 1): Promise<ProductResponse> {
+export function getProducts (): Promise<ProductResponse> {
   // const params = { limit, offset: (page - 1) * limit }
   //   const params = null
   return request.get<ProductResponse>('/fid-task-4-ffront-products.json')
