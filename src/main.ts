7ef5d5bad10a572a81e911620b4b5a8407173d14
yxj0312 <yanxiajun@hotmail.com>
2021-02-03 23:04:40 +0100

    Change some configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import Harlem from '@harlem/core';
-import './assets/styles/index.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import Harlem from '@harlem/core'
+import './assets/styles/index.css'
 
-createApp(App).use(router).use(Harlem).mount('#app');
+createApp(App).use(router).use(Harlem).mount('#app')
