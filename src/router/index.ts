eccd3a0240fb1582effd6b6c14bedb001c897cad
yxj0312 <yanxiajun@hotmail.com>
2021-06-03 23:01:22 +0200

    update home location

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,25 +4,21 @@ import { App } from 'vue'
 
 import Home from "../views/Home.vue";
 
-const routes: Array<RouteRecordRaw> = [
+export const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
+    path: '/',
+    name: 'Layout',
+    // redirect: '/dashboard',
+    component: () => import(/* webpackChunkName: "layout" */ '@/layout/index.vue'),
+    meta: {
+      title: '首页'
+    },
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // process.env.BASE_URL
+  history: createWebHistory(),
   routes,
 });
 
