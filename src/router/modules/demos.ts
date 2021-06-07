72cf0143086002fb7f5d94e40fef319e8c531697
yxj0312 <yanxiajun@hotmail.com>
2021-06-07 23:14:39 +0200

    comment some unused component

--- src/router/modules/demos.ts
+++ src/router/modules/demos.ts
@@ -1,5 +1,5 @@
 import { RouteRecordRaw } from 'vue-router'
-import { RouterTransition } from '@/components/transition'
+// import { RouterTransition } from '@/components/transition'
 
 const routeName = 'demos'
 
@@ -8,7 +8,7 @@ const routes: Array<RouteRecordRaw> = [
     path: '/demos',
     name: routeName,
     redirect: '/demos/custom-a-custom-modal',
-    component: RouterTransition,
+    // component: RouterTransition,
     meta: {
       title: 'demo演示',
       icon: 'icon-zhuomian'
